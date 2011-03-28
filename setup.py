246317d698febe609f8dc7c525fb8b8f9f6309a2
Mathieu Lecarme <mathieu@garambrogne.net>
2011-03-28 10:03:59 +0200

    setuptools

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(name='py-ucengine',
+      version='0.0.1',
+      package_dir={'': 'src'},
+      url='http://github.com/athoune/py-ucengine',
+      #scripts=[],
+      description="Python client for UCEngine",
+      license="BSD",
+      author="Mathieu Lecarme",
+      packages=['ucengine'],
+      keywords= [''],
+      zip_safe = True,
+      install_requires=["gevent"]
+      )
